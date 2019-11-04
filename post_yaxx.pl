bdfe651617a73b447f08390f2c0bc681474f6782
bspitzbart <bradley.spitzbart@stonybrook.edu>
2019-11-04 03:47:48 -0500

    Update post_yaxx.pl
    Add comments

--- post_yaxx.pl
+++ post_yaxx.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-
+# this script collects all the individual results to produce a uniform database
 $nargs=2;
 $proc="-f";
 if ($ARGV[0] eq "-p" || $ARGV[0] eq "-f" || $ARGV[0] eq "-r" || $ARGV[0] eq "-s" || $ARGV[0] eq "-t") {
@@ -42,19 +42,19 @@ $prog_dir="/data/ANCHORS/YAXX/bin_linux";
 if ($proc eq "-f") {
   # creates counts.fits
   print "run_dmextract.pl $ARGV[0] $ARGV[1] $ARGV[2]\n";
-  `$prog_dir/run_dmextract.pl $ARGV[0] $ARGV[1] $ARGV[2]`;
+  `$prog_dir/run_dmextract.pl $ARGV[0] $ARGV[1] $ARGV[2]`; # find sources
 
   print "run_dmcopy.pl $ARGV[0] $ARGV[1] $ARGV[2]\n";
-  `$prog_dir/run_dmcopy.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/run_dmcopy.pl $ARGV[0] $ARGV[1]`;  # make thumbnail plots
 
   print "run_bblocks.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/run_bblocks.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/run_bblocks.pl $ARGV[0] $ARGV[1]`;  # calculate bayesian stats
 
   print "run_gl.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/run_gl.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/run_gl.pl $ARGV[0] $ARGV[1]`;  # calculate GLvary stats
 
   print "run_quantile.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/run_quantile.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/run_quantile.pl $ARGV[0] $ARGV[1]`;  # calculate quantiles
 
   # run idl programs plot_bblocks.pro and plot_spectra.pro  ...
   open(IDL,">run_idl_plots.pro");
@@ -63,17 +63,17 @@ if ($proc eq "-f") {
   print IDL "GLvary_plot,\'$ARGV[0]\', \'$ARGV[1]\'\n";
   print IDL "exit\n";
   close IDL;
-  `idl run_idl_plots`;
+  `idl run_idl_plots`;  # make plots
 
   print "collect_skyview.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/collect_skyview.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/collect_skyview.pl $ARGV[0] $ARGV[1]`;  # look for other data sources
 
   print "collect_img_to_jpg.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/collect_img_to_jpg.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/collect_img_to_jpg.pl $ARGV[0] $ARGV[1]`;  # convert plots for web display
 
   # do 2MASS matching
   print "run_2mass.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/run_2mass.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/run_2mass.pl $ARGV[0] $ARGV[1]`;  # look for IR data sources
 }  # if ($proc eq "-f") {
 
 
@@ -89,22 +89,22 @@ if ($proc eq "-r" || $proc eq "-s") {
 
 if ($proc eq "-p" || $proc eq "-f" || $proc eq "-r" || $proc eq "-s") {
   print "collect_target_table.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/collect_target_table.pl $ARGV[0] $ARGV[1] $ARGV[2]`;
+  `$prog_dir/collect_target_table.pl $ARGV[0] $ARGV[1] $ARGV[2]`;  # aggregate all sources
 
   print "assemble_report.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/assemble_report.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/assemble_report.pl $ARGV[0] $ARGV[1]`;  # make postscript report
 
   print "merge2web.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/merge2web.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/merge2web.pl $ARGV[0] $ARGV[1]`;  # publish to website
 }  # if ($proc eq "-f" || $proc eq "-r" || $proc eq "-s") {
 
 if ($proc eq "-p" || $proc eq "-f" || $proc eq "-r" || $proc eq "-t") {
   print "assemble_csv.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/assemble_csv.pl $ARGV[0] $ARGV[1]`;
-  `$prog_dir/assemble_apec.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/assemble_csv.pl $ARGV[0] $ARGV[1]`;  # make csv file for user download
+  `$prog_dir/assemble_apec.pl $ARGV[0] $ARGV[1]`;  # make csv file for updated fits
 
   print "assemble_obs.pl $ARGV[0] $ARGV[1]\n";
-  `$prog_dir/assemble_obs.pl $ARGV[0] $ARGV[1]`;
+  `$prog_dir/assemble_obs.pl $ARGV[0] $ARGV[1]`;  # add to total aggregate list
 }  # if ($proc eq "-f" || $proc eq "-r" || $proc eq "-t") {
 
 #end
